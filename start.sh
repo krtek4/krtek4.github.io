fbad0a58dd0a98cf8a380219cb0d9fa094706394
Gilles Crettenand <gilles.crettenand@liip.ch>
2018-04-11 16:36:48 +0200

    use jekyll livereload

--- start.sh
+++ start.sh
@@ -2,21 +2,10 @@
 
 BUNDLE=$(which bundle)
 RBENV=$(which rbenv)
-NPM=$(which npm)
 
 if [[ -z "$BUNDLE" ]]; then
     echo "Please install bundler"
     exit 1
-fi
-
-if [[ -z "$NPM" ]]; then
-    echo "Please install npm"
-    exit 1
-fi
-
-BROWSERSYNC="./node_modules/.bin/browser-sync"
-if [[ ! -x "$BROWSERSYNC" ]]; then
-    ${NPM} install
 fi
 
 JEKYLL="./.bundle/packages/ruby/2.5.0/bin/jekyll"
@@ -24,10 +13,9 @@ if [[ ! -x "$JEKYLL" ]]; then
     ${BUNDLE} install --path .bundle/packages
 fi
 
-COMMAND="bundle exec jekyll serve"
+COMMAND="bundle exec jekyll serve --livereload --baseurl ''"
 if [[ -n "$RBENV" ]]; then
     COMMAND="$RBENV exec $COMMAND"
 fi
 
-${COMMAND} --baseurl / &
-${BROWSERSYNC} start --files "_site/" --proxy "localhost:4000" --reload-debounce 600
+${COMMAND}
